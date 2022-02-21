156f91c73c6841d875d22ae71cb98a661353a0bd
SANAYEE <dudtkd0219@gmail.com>
2022-02-22 04:00:09 +0900

    이진탐색

--- src/LV3/SteppingStones.java
+++ src/LV3/SteppingStones.java
@@ -1,7 +1,9 @@
 package LV3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class SteppingStones {
 	public int solution(int[] stones, int k) {
@@ -14,16 +16,46 @@ public class SteppingStones {
         	}
         	map.get(stones[i]).add(i);
         }
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        ArrayList<Integer> go = new ArrayList<>();
         
-        for(int key : map.keySet()) {
+        Object[] keySet = map.keySet().toArray();
+        Arrays.sort(keySet);
+        
+        Object key = 0;
+        for(int i = keySet.length-1; i >= 0; i--) {
+        	key = keySet[i];
+        	ArrayList<Integer> exam = new ArrayList<>();
         	System.out.println("값 : " +key + " 이 있는 인덱스 들 : " +map.get(key));
+        	System.out.println("들어가기 전 리스트 : " +go);
+        	System.out.println("↑ 여기에 들어갈 리스트 : " +map.get(key));
+        	go.addAll(map.get(key));
+        	que.addAll(go);
+        	
+            while(!que.isEmpty()) {
+            	exam.add(que.poll());
+            }
+            System.out.println(exam);
         }
-        
         
         
         
         return answer;
     }
+	
+	public static void gogo(ArrayList<Integer> go, ArrayList<Integer> mapGet) {
+		
+		int key = 0;
+		int mid = 0;
+		int low = 0, high = 0;
+		
+		for(int i = 0; i < mapGet.size(); i++) {
+			
+			
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		SteppingStones s = new SteppingStones();
