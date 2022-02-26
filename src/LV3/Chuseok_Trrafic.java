3a4878e67eda8dd75393dbc2d8ba707d61a89d83
zzomi <zzomi@211.228.124.62>
2022-02-26 10:57:44 +0900

    커밋 테스트

--- src/LV3/Chuseok_Trrafic.java
+++ src/LV3/Chuseok_Trrafic.java
@@ -30,7 +30,7 @@ public class Chuseok_Trrafic {
 		if(start < 0) {
 			start += 60;
 			startMin -= 1;
-			if(startMin < 0) {
+			if(startMin < 0) { 
 				startMin += 60;
 				startHour -= 1;
 				if(startHour < 0) { 
