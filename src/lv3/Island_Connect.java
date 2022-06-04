78d24ca0b8aa1f96f97a187ed81629eadb3aebf6
SANAYEE <dudtkd0219@gmail.com>
2022-06-04 21:09:38 +0900

    어떻게 접근해야 될까

--- src/lv3/Island_Connect.java
+++ src/lv3/Island_Connect.java
@@ -11,7 +11,7 @@ public class Island_Connect {
         	map[i[0]][i[1]] = i[2];
         	map[i[1]][i[0]] = i[2];
         }
-        print(map);
+        print(map); 
         
         int distance = 0;
         int targetIndex = 0;
