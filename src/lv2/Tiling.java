bfcf78960d5d64b6e60a5758309632b956731116
SANAYEE <dudtkd0219@gmail.com>
2022-06-06 23:53:08 +0900

    3xn tiling

---
+++ src/lv2/Tiling.java
@@ -0,0 +1,19 @@
+package lv2;
+
+public class Tiling {
+	public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		Tiling s = new Tiling();
+		
+		System.out.println("test case input 1 : " +s.solution(1));
+		System.out.println("test case input 2 : " +s.solution(2));
+		System.out.println("test case input 3 : " +s.solution(3));
+		System.out.println("test case input 4 : " +s.solution(4));
+
+	}
+
+}
