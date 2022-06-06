3653cfee8ff9a51edada3b550b38ace163b2c73c
SANAYEE <dudtkd0219@gmail.com>
2022-06-07 00:12:59 +0900

    좀더

--- src/lv2/Tiling.java
+++ src/lv2/Tiling.java
@@ -1,9 +1,39 @@
 package lv2;
 
 public class Tiling {
-	public int solution(int n) {
+	
+	public int n2(int n) {
         int answer = 0;
+        int sum = 0;
+		int a = 0;
+        int b = 1;
+		
+		
+		if (n == 1) {
+            answer = 1;
+        }
+		else if(n == 2) {
+			answer = 2;
+		}
+        else {
+            for (int i = 2; i <= n; i++) {
+                sum = (a + b)%1000000007;
+                a = b;
+                b = sum;
+            }
+            answer = (a + b)%1000000007;
+        }
         return answer;
+    }
+	
+	public int solution(int n) {
+		int answer = 0;
+        if(n%2 == 1){
+            return 0;
+        }
+        else{
+            return answer;
+        }
     }
 
 	public static void main(String[] args) {
