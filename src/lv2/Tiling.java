068b1fd4ba4c617802e66fcac2f467b2c454b7b9
SANAYEE <dudtkd0219@gmail.com>
2022-06-09 02:37:09 +0900

    미루기

--- src/lv2/Tiling.java
+++ src/lv2/Tiling.java
@@ -7,46 +7,83 @@ public class Tiling {
 		long down = 1;
 		if(n-r <= r) {
 			for(long i = n; i >= r+1; i--) {
-				up *= i;
+				up = (up*i)%1000000007;
 			}
 			for(long i = n-r; i >= 1; i--) {
-				down *= i;
+				down = (down*i)%1000000007;
 			}
 			return up/down;
 		}
 		else {
 			for(long i = n; i >= n-r+1; i--) {
-				up *= i;
+				up = (up*i)%1000000007;
 			}
 			for(long i = r; i >= 1; i--) {
-				down *= i;
+				down = (down*i)%1000000007;
 			}
 			return up/down;
 		}
 	}
 	
+	public long firstStage(int cnt) {
+		Tiling t = new Tiling();
+		long value = 0;
+		
+		for(int i = cnt; i >= 0; i--) {
+			value +=( ((long) Math.pow(2, i))%1000000007 * (t.comb((long) cnt, (long) i)) );
+		}
+		
+		return value;
+	}
+	
+	public long secondStage(int n, int cnt) {
+		Tiling t = new Tiling();
+		long value = 0;
+		int fourMax = n/4;
+		int one = 0;
+
+		int twoMax = 0;
+		if(n % 4 == 2) {
+			twoMax = n/cnt+1;
+		}
+		else if(n % 4 == 0) {
+			twoMax = n/cnt;
+		}
+		System.out.println(fourMax + "  " + twoMax);
+		for(int i = 1; i <= fourMax; i++) {
+			for(int j = twoMax; j >= 0; j --) {
+				System.out.println("4's 밑 : " +i + " // 2's 밑 : " + j + " // 1's 밑 : " +one);
+				value += ((long)(Math.pow(2, i)%1000000007 ) * 
+						  (long)(Math.pow(2, j)%1000000007 ) * 
+						  (long)(Math.pow(1, one)%1000000007 ) *
+						  (long)(t.comb((long) cnt-1, (long) j)%1000000007) * 
+						  (long)(t.comb((long) cnt-1, (long) one)%1000000007));
+				one++;
+			}
+			one = 0;
+			twoMax -= 2;
+		}
+		
+		
+		
+		return value;
+	}
+	
 	public int solution(int n) {
-		long answer = 0;
-		long jaeGob = 0;
-		long cnt = n/2;
-		long under = cnt + 1;
-		//System.out.println("시작 n : " +under + " 시작 r : " +(cnt-1));
-        if(n%2 == 1){
-            return 0;
-        }
-        else if(n == 2) {
-        	return 1;
-        }
-        else{
-        	jaeGob = (long) Math.pow(2, cnt);
-        	
-        	for(long i = cnt-1; i >= 1; i--) {
-        		answer += comb(under,i);
-        		under++;
-        	}
-        	//System.out.println("제곱 : " +jaeGob + " 조합들 : " +answer);
-            return (int) (jaeGob + answer*2 + 1)%1000000007;
-        }
+		int cnt = n/2;
+		int answer = 0;
+		if(n % 2 == 1) {
+			return 0;
+		}
+		else if(n == 2) {
+			return 3;
+		}
+		else {
+			System.out.println("firstStage : " + firstStage(cnt));
+			answer += firstStage(cnt);
+			answer += secondStage(n, cnt);
+			return answer;
+		}
     }
  
 	public static void main(String[] args) {
