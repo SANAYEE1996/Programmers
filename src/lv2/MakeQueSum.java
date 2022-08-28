e5c22676060951054e5728f31a5300226649484f
SANAYEE <dudtkd0219@gmail.com>
2022-08-29 00:05:01 +0900

    스택 오버 플로우 해결해야 되네..

--- src/lv2/MakeQueSum.java
+++ src/lv2/MakeQueSum.java
@@ -4,17 +4,22 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MakeQueSum {
+	
+	int maxSize = 0;
+	
 	public int solution(int[] queue1, int[] queue2) {
-		
-        Queue<Integer> first = new LinkedList<>();
-        Queue<Integer> second = new LinkedList<>();
+		System.out.println(Long.MAX_VALUE);
+		System.out.println((long)300000*100000000);
+		maxSize = (queue1.length+queue2.length)*2;
+        Queue<Long> first = new LinkedList<>();
+        Queue<Long> second = new LinkedList<>();
         long firstSum = 0;
         long secondSum = 0;
         for(int i=0 ; i < queue1.length; i++) {
         	firstSum += (long)queue1[i];
-        	first.add(queue1[i]);
+        	first.add((long)queue1[i]);
         	secondSum += (long)queue2[i];
-        	second.add(queue2[i]);
+        	second.add((long)queue2[i]);
         }
         if(firstSum == secondSum) {
         	return 0;
@@ -30,41 +35,21 @@ public class MakeQueSum {
         return chance[0];
     }
 	
-	void getFirstToSecond(Queue<Integer> first, Queue<Integer> second,
+	void getFirstToSecond(Queue<Long> first, Queue<Long> second,
 						  long firstSum, long secondSum, int[] chance) {
-		if(chance[0] > (first.size()+second.size())*2) {
+		if(chance[0] > maxSize) {
 			chance[0] = -1;
 			return;
 		}
-		int firstPollValue = first.poll();
-		//System.out.println("first Poll Value : " +firstPollValue);
-		second.add(firstPollValue);
-		firstSum -= (long)firstPollValue;
-		secondSum += (long)firstPollValue;
-		//System.out.println("firstSum : " +firstSum + " secondSum : " +secondSum);
-		chance[0]++;
-		if(firstSum == secondSum) {
-			return;
-		}
-		else if(firstSum < secondSum) {
+		if(first.isEmpty()) {
 			getSecondToFirst(first, second, firstSum, secondSum, chance);
-		}
-		else {
-			getFirstToSecond(first, second, firstSum, secondSum, chance);
-		}
-	}
-	
-	void getSecondToFirst(Queue<Integer> first, Queue<Integer> second,
-						  long firstSum, long secondSum, int[] chance) {
-		if(chance[0] > (first.size()+second.size())*2) {
-			chance[0] = -1;
 			return;
 		}
-		int secondPollValue = second.poll();
-//		System.out.println("second Poll Value : " +secondPollValue);
-		first.add(secondPollValue);
-		firstSum += (long)secondPollValue;
-		secondSum -= (long)secondPollValue;
+		long firstPollValue = first.poll();
+		second.add(firstPollValue);
+		firstSum -= firstPollValue;
+		secondSum += firstPollValue;
+//		System.out.println("first Poll Value : " +firstPollValue);
 //		System.out.println("firstSum : " +firstSum + " secondSum : " +secondSum);
 		chance[0]++;
 		if(firstSum == secondSum) {
@@ -77,4 +62,34 @@ public class MakeQueSum {
 			getFirstToSecond(first, second, firstSum, secondSum, chance);
 		}
 	}
+	
+	void getSecondToFirst(Queue<Long> first, Queue<Long> second,
+						  long firstSum, long secondSum, int[] chance) {
+		if(chance[0] > maxSize) {
+			chance[0] = -1;
+			return;
+		}
+		if(second.isEmpty()) {
+			getFirstToSecond(first, second, firstSum, secondSum, chance);
+			return;
+		}
+		long secondPollValue = second.poll();
+		first.add(secondPollValue);
+		firstSum += secondPollValue;
+		secondSum -= secondPollValue;
+//		System.out.println("second Poll Value : " +secondPollValue);
+//		System.out.println("firstSum : " +firstSum + " secondSum : " +secondSum);
+		chance[0]++;
+		if(firstSum == secondSum) {
+			return;
+		}
+		else if(firstSum < secondSum) {
+			getSecondToFirst(first, second, firstSum, secondSum, chance);
+		}
+		else {
+			getFirstToSecond(first, second, firstSum, secondSum, chance);
+		}
+	}
+	
+	
 }
--- src/test/IntegerationTest.java
+++ src/test/IntegerationTest.java
@@ -18,11 +18,19 @@ class IntegerationTest {
 	
 	@Test
 	void test() {
-		System.out.println(mqs.solution(new int[] {3, 2, 7, 2}, new int[] {4, 6, 5, 1}));
-		System.out.println(mqs.solution(new int[] {1, 2, 1, 2}, new int[] {1, 10, 1, 2}));
-		System.out.println(mqs.solution(new int[] {1, 1}, new int[] {1, 5}));
+		//System.out.println(mqs.solution(new int[] {3, 2, 7, 2}, new int[] {4, 6, 5, 1}));
+		//System.out.println(mqs.solution(new int[] {1, 2, 1, 2}, new int[] {1, 10, 1, 2}));
+		//System.out.println(mqs.solution(new int[] {1, 1}, new int[] {1, 5}));
 		
-		assertEquals(-1, mqs.solution(new int[] {1}, new int[] {3}));
+		int[] queue1 = new int[300000];
+		int[] queue2 = new int[300000];
+		for(int i = 0; i < 300000; i++) {
+			queue1[i] = 1;
+			queue2[i] = 1;
+		}
+		queue2[299998] = 599999;
+		//assertEquals(-1, mqs.solution(new int[] {1}, new int[] {3}));
+		System.out.println(mqs.solution(queue1, queue2));
 	}
 
 }
