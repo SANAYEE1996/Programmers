611126adca3db4e2a4f2552be91fa6f656149063
SANAYEE <dudtkd0219@gmail.com>
2022-08-31 00:51:56 +0900

    함수호출이 아닌 단순 반복문으로 하니 스택 오버플로우 발생하지 않음 // 문제 해결

--- src/lv2/MakeQueSum.java
+++ src/lv2/MakeQueSum.java
@@ -8,8 +8,6 @@ public class MakeQueSum {
 	int maxSize = 0;
 	
 	public int solution(int[] queue1, int[] queue2) {
-		System.out.println(Long.MAX_VALUE);
-		System.out.println((long)300000*100000000);
 		maxSize = (queue1.length+queue2.length)*2;
         Queue<Long> first = new LinkedList<>();
         Queue<Long> second = new LinkedList<>();
@@ -25,69 +23,36 @@ public class MakeQueSum {
         	return 0;
         }
         int[] chance = {0};
-        if(firstSum < secondSum) {
-        	getSecondToFirst(first, second, firstSum, secondSum, chance);
-        }
-        else {
-        	getFirstToSecond(first, second, firstSum, secondSum, chance);
-        }
+        caculate(chance, first, second, firstSum, secondSum);
         
         return chance[0];
     }
 	
-	void getFirstToSecond(Queue<Long> first, Queue<Long> second,
-						  long firstSum, long secondSum, int[] chance) {
-		if(chance[0] > maxSize) {
+	void caculate(int[] chance, 
+				  Queue<Long> first, Queue<Long> second,
+				  long firstSum, long secondSum) {
+		long pollValue = 0;
+		while(maxSize > 0) {
+			if(firstSum == secondSum) {
+				return;
+			}
+			else if(firstSum < secondSum) {
+				pollValue = second.poll();
+				first.add(pollValue);
+				firstSum += pollValue;
+				secondSum -= pollValue;
+			}
+			else {
+				pollValue = first.poll();
+				second.add(pollValue);
+				firstSum -= pollValue;
+				secondSum += pollValue;
+			}
+			chance[0]++;
+			maxSize--;
+		}
+		if(firstSum != secondSum) {
 			chance[0] = -1;
-			return;
-		}
-		if(first.isEmpty()) {
-			getSecondToFirst(first, second, firstSum, secondSum, chance);
-			return;
-		}
-		long firstPollValue = first.poll();
-		second.add(firstPollValue);
-		firstSum -= firstPollValue;
-		secondSum += firstPollValue;
-//		System.out.println("first Poll Value : " +firstPollValue);
-//		System.out.println("firstSum : " +firstSum + " secondSum : " +secondSum);
-		chance[0]++;
-		if(firstSum == secondSum) {
-			return;
-		}
-		else if(firstSum < secondSum) {
-			getSecondToFirst(first, second, firstSum, secondSum, chance);
-		}
-		else {
-			getFirstToSecond(first, second, firstSum, secondSum, chance);
-		}
-	}
-	
-	void getSecondToFirst(Queue<Long> first, Queue<Long> second,
-						  long firstSum, long secondSum, int[] chance) {
-		if(chance[0] > maxSize) {
-			chance[0] = -1;
-			return;
-		}
-		if(second.isEmpty()) {
-			getFirstToSecond(first, second, firstSum, secondSum, chance);
-			return;
-		}
-		long secondPollValue = second.poll();
-		first.add(secondPollValue);
-		firstSum += secondPollValue;
-		secondSum -= secondPollValue;
-//		System.out.println("second Poll Value : " +secondPollValue);
-//		System.out.println("firstSum : " +firstSum + " secondSum : " +secondSum);
-		chance[0]++;
-		if(firstSum == secondSum) {
-			return;
-		}
-		else if(firstSum < secondSum) {
-			getSecondToFirst(first, second, firstSum, secondSum, chance);
-		}
-		else {
-			getFirstToSecond(first, second, firstSum, secondSum, chance);
 		}
 	}
 	
